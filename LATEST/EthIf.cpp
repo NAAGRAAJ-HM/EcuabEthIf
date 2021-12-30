b2a5669dc11b134e9396f679bc5e485272710c75
root <nagaraja.hm.0001@gmail.com>
2021-12-30 21:33:01 +0100

    Update: submodule EthIf

---
+++ LATEST/EthIf.cpp
@@ -0,0 +1,7 @@
+#include "EthIf.h"
+
+class_EthIf EthIf;
+
+void class_EthIf::InitFunction(void){
+}
+
---
+++ LATEST/EthIf.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_EthIf{
+   public:
+      void InitFunction(void);
+};
+
+extern class_EthIf EthIf;
+
