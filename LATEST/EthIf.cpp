b5b10a74b77caad464b2e72e585f391df31c2965
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:03:01 +0200

    Update: AUTOSAR basic software ECU abstraction module EthIf

--- LATEST/EthIf.cpp
+++ LATEST/EthIf.cpp
@@ -37,10 +37,9 @@ class module_EthIf:
    public:
       module_EthIf(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, ETHIF_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, ETHIF_CONFIG_DATA, ETHIF_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, ETHIF_CODE) InitFunction      (void);
       FUNC(void, ETHIF_CODE) DeInitFunction    (void);
       FUNC(void, ETHIF_CODE) MainFunction      (void);
 
@@ -81,23 +80,39 @@ VAR(module_EthIf, ETHIF_VAR) EthIf(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, ETHIF_CODE) module_EthIf::InitFunction(
-   CONSTP2CONST(CfgEthIf_Type, CFGETHIF_CONFIG_DATA, CFGETHIF_APPL_CONST) lptrCfgEthIf
+   CONSTP2CONST(CfgModule_TypeAbstract, ETHIF_CONFIG_DATA, ETHIF_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgEthIf){
+   if(E_OK == IsInitDone){
 #if(STD_ON == EthIf_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgEthIf for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == EthIf_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_EthIf as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   EthIf.IsInitDone = E_OK;
 }
 
 FUNC(void, ETHIF_CODE) module_EthIf::DeInitFunction(void){
-   EthIf.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == EthIf_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, ETHIF_CODE) module_EthIf::MainFunction(void){
