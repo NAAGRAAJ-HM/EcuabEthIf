ed405f302cd272851a933f4086e79075f93dae27
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:24:16 +0000

    ECU abstraction Update: AUTOSAR basic software module EthIf

--- LATEST/EthIf.cpp
+++ LATEST/EthIf.cpp
@@ -105,8 +105,8 @@ FUNC(void, ETHIF_CODE) module_EthIf::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgEthIf;
          }
       }
       IsInitDone = E_OK;
