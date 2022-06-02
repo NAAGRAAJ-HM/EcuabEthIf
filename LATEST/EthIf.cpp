93482bc2deac0569a72c8b54702b66abc7d228fd
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-02 12:06:02 +0200

    Update: AUTOSAR ECU abstraction submodule - EthIf

--- LATEST/EthIf.cpp
+++ LATEST/EthIf.cpp
@@ -76,6 +76,10 @@ CONSTP2VAR(infSchMClient, ETHIF_VAR, ETHIF_CONST) gptrinfSchMClient_EthIf = &Eth
 /******************************************************************************/
 VAR(module_EthIf, ETHIF_VAR) EthIf(
    {
+#if(STD_ON == _ReSIM)
+// char strModuleName[6];
+#else
+#endif
          ETHIF_AR_RELEASE_VERSION_MAJOR
       ,  ETHIF_AR_RELEASE_VERSION_MINOR
       ,  0x00
