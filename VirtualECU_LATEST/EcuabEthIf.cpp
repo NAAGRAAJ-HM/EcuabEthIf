59cb6dcdfa9a484e2f3c8ddc9f903b987d74fb23
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:04:55 +0200

    Update: AUTOSAR basic software ECU abstraction module for ethernet communication protocol stack - interface layer

--- VirtualECU_LATEST/EcuabEthIf.cpp
+++ VirtualECU_LATEST/EcuabEthIf.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : EcuabEthIf.cpp                                                         */
+/* File   : EcuabEthIf.cpp                                                    */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define ECUABETHIF_AR_RELEASE_VERSION_MAJOR                                         4
-#define ECUABETHIF_AR_RELEASE_VERSION_MINOR                                         3
+#define ECUABETHIF_AR_RELEASE_VERSION_MAJOR                                    4
+#define ECUABETHIF_AR_RELEASE_VERSION_MINOR                                    3
 
 /******************************************************************************/
 /* MACROS                                                                     */
