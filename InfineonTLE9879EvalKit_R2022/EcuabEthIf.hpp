96bf3252b4b8f9965cc45618bda0cb9a6f6dc5c4
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-29 15:52:39 +0200

    Update: AUTOSAR basic software ECU abstraction module for ethernet communication protocol stack - interface layer

---
+++ InfineonTLE9879EvalKit_R2022/EcuabEthIf.hpp
@@ -0,0 +1,71 @@
+#pragma once
+/******************************************************************************/
+/* File   : EcuabEthIf.hpp                                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstEcuabEthIf.hpp"
+#include "CfgEcuabEthIf.hpp"
+#include "EcuabEthIf_core.hpp"
+#include "infEcuabEthIf_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_EcuabEthIf:
+      INTERFACES_EXPORTED_ECUABETHIF
+      public abstract_module
+   ,  public class_EcuabEthIf_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstEcuabEthIf_Type* lptrConst = (ConstEcuabEthIf_Type*)NULL_PTR;
+      infServicePduRClient_Lo infServicePduRClient_EcuabEthIf;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, ECUABETHIF_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, ECUABETHIF_CONST,       ECUABETHIF_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   ECUABETHIF_CONFIG_DATA, ECUABETHIF_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, ECUABETHIF_CODE) DeInitFunction    (void);
+      FUNC(void, ECUABETHIF_CODE) MainFunction      (void);
+      ECUABETHIF_CORE_FUNCTIONALITIES
+
+      FUNC(void, ECUABETHIF_CODE) MainFunctionRx    (void);
+      FUNC(void, ECUABETHIF_CODE) MainFunctionTx    (void);
+      FUNC(void, ECUABETHIF_CODE) MainFunctionState (void);
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_EcuabEthIf, ECUABETHIF_VAR) EcuabEthIf;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
