9c9e668f6d0a418c9a87a75ce52d75ba594c9924
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:31:24 +0100

    Update: AUTOSAR basic software ECU abstraction module for ethernet communication protocol stack - interface layer

--- InfineonTLE9879EvalKit_R2023/infEcuabEthIf_ServiceNvM_Types.hpp
+++ InfineonTLE9879EvalKit_R2023/infEcuabEthIf_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_EcuabEthIf_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_EcuabEthIf_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
